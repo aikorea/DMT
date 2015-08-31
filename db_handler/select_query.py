8676b71dafa1c6d8a3e9a593db561ab6b053c887
Dalgu <dalgu90@gmail.com>
2015-09-01 08:55:28 +0900

    select_query.py json output option

--- db_handler/select_query.py
+++ db_handler/select_query.py
@@ -5,6 +5,7 @@ import argparse
 parser = argparse.ArgumentParser(description='JSON files uploader')
 parser.add_argument('type')
 parser.add_argument('query')
+parser.add_argument('--output', help='output JSON filename')
 args = parser.parse_args()
 
 args.query = json.loads(args.query)
@@ -13,5 +14,14 @@ with open('conf.json', 'r') as fd:
     conf = json.load(fd)
 db = mydb.init(conf)
 res = mydb.find(db, args)
-for val in res:
-    print val
+
+if args.output:
+	langs = {"en", "kr"}
+	parcors = [{key:value for (key, value) in val.items() if key in langs}\
+				for val in res]
+	out = {"source":args.query['source'], "type":args.type, "values":parcors}
+	with open(args.output, 'w') as fd:
+		json.dump(out, fd, indent=2)
+else:
+	for val in res:
+    		print val
